6883d069f3a702f2ec6b9ee6c4be82a2005a8a37
ChinonsoIg <ojononso@yahoo.com>
2022-10-29 12:34:18 +0100

    design the anchor buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import ProfileImage from './assets/images/profile_img.svg';
 import ShareBtn from './assets/images/share_btn.svg';
 import ShareBtnMobile from './assets/images/share_btn_mobile.svg';
+import SlackIcon from './assets/images/slack_icon.svg';
+import GithubIcon from './assets/images/github_icon.svg';
 
 function App() {
   const handleClick = () => {
@@ -16,6 +18,18 @@ function App() {
           <img src={ShareBtn} alt="Share button desktop" className="share_btn" />
           <img src={ShareBtnMobile} alt="Share button mobile" className="share_btn_mobile" />
         </header>
+        <section className="links">
+          <a href="https://training.zuri.team/" target="_blank" rel="noreferrer" id="btn__zuri">Zuri Training</a>
+          <a href="http://books.zuri.team" target="_blank" rel="noreferrer" id="books">Zuri Team</a>
+          <a href="https://background.zuri.team/python-for-beginners?ref_id=Chinonso" target="_blank" rel="noreferrer" id="book__python">Python Book</a>
+          <a href="https://background.zuri.team" target="_blank" rel="noreferrer" id="pitch">Pitch</a>
+          <a href="https://books.zuri.team/design-rules" target="_blank" rel="noreferrer" id="book__design">Design Book</a>
+          <div className="social_icons">
+            <img src={SlackIcon} alt="Slack icon" className="social_icon" />
+            <img src={GithubIcon} alt="Github icon" className="social_icon" />
+          </div>
+        </section>
+        
       </main>
     </div>
   );
