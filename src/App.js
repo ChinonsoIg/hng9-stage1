f0e7ac72f2daef571fa322968d6015ff799b61a2
ChinonsoIg <ojononso@yahoo.com>
2022-10-30 01:25:42 +0100

    add subtext to button links

--- src/App.js
+++ src/App.js
@@ -21,14 +21,33 @@ function App() {
           <img src={ShareBtnMobile} alt="Share button mobile" className="share_btn_mobile" />
         </header>
         <section className="links">
-          <a href="https://training.zuri.team/" target="_blank" rel="noreferrer" id="btn__zuri">Zuri Training</a>
-          <a href="http://books.zuri.team" target="_blank" rel="noreferrer" id="books">Zuri Team</a>
-          <a href="https://background.zuri.team/python-for-beginners?ref_id=Chinonso" target="_blank" rel="noreferrer" id="book__python">Python Book</a>
-          <a href="https://background.zuri.team" target="_blank" rel="noreferrer" id="pitch">Pitch</a>
-          <a href="https://books.zuri.team/design-rules" target="_blank" rel="noreferrer" id="book__design">Design Book</a>
+          <a href="https://training.zuri.team/" target="_blank" rel="noreferrer" id="btn__zuri">
+            Zuri Training
+            <sub>Join the Zuri team.</sub>
+          </a>
+          <a href="http://books.zuri.team" target="_blank" rel="noreferrer" id="books">
+            Zuri Team
+            <sub>Find books about design and coding.</sub>
+          </a>
+          <a href="https://background.zuri.team/python-for-beginners?ref_id=Chinonso" target="_blank" rel="noreferrer" id="book__python">
+            Python Book
+            <sub>Best books on Python from beginners to expert.</sub>
+          </a>
+          <a href="https://background.zuri.team" target="_blank" rel="noreferrer" id="pitch">
+            Background checks on Coders
+          <sub>We help you verify coders.</sub>
+          </a>
+          <a href="https://books.zuri.team/design-rules" target="_blank" rel="noreferrer" id="book__design">
+            Design Book
+            <sub>To understand design rules better, get our book for free.</sub>
+          </a>
           <div className="social_icons">
-            <img src={SlackIcon} alt="Slack icon" className="social_icon" />
-            <img src={GithubIcon} alt="Github icon" className="social_icon" />
+            <a href="https://hng9.slack.com" className="exclude_link" target="_blank" rel="noreferrer">
+              <img src={SlackIcon} alt="Slack icon" className="social_icon" />
+            </a>
+            <a href="https://github.com/ChinonsoIg" className="exclude_link" target="_blank" rel="noreferrer">
+              <img src={GithubIcon} alt="Github icon" className="social_icon" />
+            </a>
           </div>
         </section>
       </main>
