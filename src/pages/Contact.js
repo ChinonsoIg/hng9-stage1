5fbc1b93564ebcb3ea83ca283b035e9332cacd5f
ChinonsoIg <ojononso@yahoo.com>
2022-11-04 10:38:23 +0100

    design error text

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Contact.css";
 
 const Contact = () => {
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [messageError, setMessageError] = useState("");
+
+  
+
   return (
     <div className="contact_container">
       <h1 className="heading">Contact Me</h1>
@@ -17,6 +24,7 @@ const Contact = () => {
                   type="text"
                   placeholder="Enter your first name"
                 />
+                <sub className={`${firstNameError ? "first_name_error" : "validation_erorr"}`}>{firstNameError}</sub>
               </div>
               <div className="flex_item">
                 <label for="last_name">Last name</label>
@@ -25,15 +33,18 @@ const Contact = () => {
                   type="text"
                   placeholder="Enter your last name"
                 />
+                <sub className={`${lastNameError ? "last_name_error" : "validation_erorr"}`}>{lastNameError}</sub>
               </div>
             </div>
             <div className="flex_item">
               <label for="email">Email</label>
               <input id="email" type="email" placeholder="yourname@email.com" />
+              <sub className={`${emailError ? "email_error" : "validation_erorr"}`}>{emailError}</sub>
             </div>
             <div className="flex_item">
               <label for="message">Message</label>
               <textarea id="message" type="text" placeholder="Send me a message and I'll reply you as soon as possible..." />
+              <sub className={`${messageError ? "message_error": "validation_erorr"}`}>{messageError}</sub>
             </div>
             <div className="checkbox_item">
               <input type="checkbox" id="agree" name="agree" value="Agree" />
