7a4907f43007de8514d2f3615e2ac3bfae66ab76
ChinonsoIg <ojononso@yahoo.com>
2022-11-04 23:56:02 +0100

    implement form validation

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,56 +2,163 @@ import React, { useState } from "react";
 import "../styles/Contact.css";
 
 const Contact = () => {
+  // const [data, setData] = useState({
+  //   firstName: "",
+  //   lastName: "",
+  //   email: "",
+  //   message: "",
+  // });
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   const [firstNameError, setFirstNameError] = useState("");
   const [lastNameError, setLastNameError] = useState("");
   const [emailError, setEmailError] = useState("");
   const [messageError, setMessageError] = useState("");
 
-  
+  // const handleChange = (evt) => {
+  //   const { name, value } = evt.target;
+  //   console.log(name);
+  //   console.log("val: ",value);
+
+  //   setData({
+  //     ...data,
+  //     [name]: value
+  //   })
+
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!firstName) {
+      setFirstNameError("First name is required");
+    }
+
+    if (!lastName) {
+      setLastNameError("Last name is required");
+    }
+
+    if (!email) {
+      setEmailError("Email is required");
+    }
+
+    if (!message) {
+      setMessageError("Please enter a message");
+    }
+    
+    console.log("data: ", firstName);
+    console.log("data: ", lastName);
+    console.log("data: ", email);
+    console.log("data: ", message);
+  };
 
   return (
     <div className="contact_container">
       <h1 className="heading">Contact Me</h1>
-      <p className="heading_description">Hi there, contact me to ask me about anything you have in mind.</p>
+      <p className="heading_description">
+        Hi there, contact me to ask me about anything you have in mind.
+      </p>
       <section>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form_container">
             <div className="name_container">
               <div className="flex_item">
-                <label for="first_name">First name</label>
+                <label htmlFor="first_name">First name</label>
                 <input
                   id="first_name"
                   type="text"
                   placeholder="Enter your first name"
+                  defaultValue={firstName}
+                  onChange={(e) => {
+                    setFirstNameError("");
+                    setFirstName(e.target.value)
+                  }}
                 />
-                <sub className={`${firstNameError ? "first_name_error" : "validation_erorr"}`}>{firstNameError}</sub>
+                <sub
+                  className={`${
+                    firstNameError ? "first_name_error" : "validation_erorr"
+                  }`}
+                >
+                  {firstNameError}
+                </sub>
               </div>
               <div className="flex_item">
-                <label for="last_name">Last name</label>
+                <label htmlFor="last_name">Last name</label>
                 <input
                   id="last_name"
                   type="text"
                   placeholder="Enter your last name"
+                  defaultValue={lastName}
+                  onChange={(e) => {
+                    setLastNameError("");
+                    setLastName(e.target.value)
+                  }}
                 />
-                <sub className={`${lastNameError ? "last_name_error" : "validation_erorr"}`}>{lastNameError}</sub>
+                <sub
+                  className={`${
+                    lastNameError ? "last_name_error" : "validation_erorr"
+                  }`}
+                >
+                  {lastNameError}
+                </sub>
               </div>
             </div>
             <div className="flex_item">
-              <label for="email">Email</label>
-              <input id="email" type="email" placeholder="yourname@email.com" />
-              <sub className={`${emailError ? "email_error" : "validation_erorr"}`}>{emailError}</sub>
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                type="email"
+                placeholder="yourname@email.com"
+                defaultValue={email}
+                onChange={(e) => {
+                  setEmailError("");
+                  setEmail(e.target.value);
+                }}
+              />
+              <sub
+                className={`${emailError ? "email_error" : "validation_erorr"}`}
+              >
+                {emailError}l
+              </sub>
             </div>
             <div className="flex_item">
-              <label for="message">Message</label>
-              <textarea id="message" type="text" placeholder="Send me a message and I'll reply you as soon as possible..." />
-              <sub className={`${messageError ? "message_error": "validation_erorr"}`}>{messageError}</sub>
+              <label htmlFor="message">Message</label>
+              <textarea
+                id="message"
+                type="text"
+                placeholder="Send me a message and I'll reply you as soon as possible..."
+                defaultValue={message}
+                onChange={(e) => {
+                  setMessageError("");
+                  setMessage(e.target.value);
+                }}
+              />
+              <sub
+                className={`${
+                  messageError ? "message_error" : "validation_erorr"
+                }`}
+              >
+                {messageError}
+              </sub>
             </div>
             <div className="checkbox_item">
-              <input type="checkbox" id="agree" name="agree" value="Agree" />
-              <label for="agree">You agree to providing your data to Chinonso who may contact you.</label>
+              <input type="checkbox" id="agree" name="agree" defaultValue="Agree" />
+              <label htmlFor="agree">
+                You agree to providing your data to Chinonso who may contact
+                you.
+              </label>
             </div>
           </div>
-          <button type="submit" id="btn_submit">Send message</button>
+          <button
+            type="submit"
+            id="btn_submit"
+            >
+            Send message
+          </button>
         </form>
       </section>
     </div>
