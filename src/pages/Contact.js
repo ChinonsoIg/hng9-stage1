cddaea95165b080bc01a31b8bad1ad0e666cc3f9
ChinonsoIg <ojononso@yahoo.com>
2022-11-04 02:08:27 +0100

    built contact header

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import "../styles/Contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="contact_container">
+      <p className="contact_me">Contact Me</p>
+      <p>Hi there, contact me to ask me about anything you have in mind.</p>
+    </div>
   )
 }
 
